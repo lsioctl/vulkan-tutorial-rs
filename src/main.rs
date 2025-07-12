e3d136d1e6b1406c54f09c1041f279236a319247
lsioctl <lsioctl@gmail.com>
2025-07-12 19:41:46 -0400

    chore: instance with validation layer, wip

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,25 @@ use winit::{
     window::{Window, WindowId},
 };
 
+use winit::raw_window_handle::HasDisplayHandle;
+
+use ash::{
+    ext::debug_utils,
+    khr::{surface, swapchain as khr_swapchain},
+    vk, Device, Entry, Instance,
+};
+
+use std::{
+    ffi::{CStr, CString},
+    os::raw::{c_char, c_void},
+};
+
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
+
+const ENABLE_VALIDATION_LAYERS: bool = true;
+
+const REQUIRED_LAYERS: [&str; 1] = ["VK_LAYER_KHRONOS_validation"];
 
 fn main() {
    pretty_env_logger::init();
@@ -24,7 +41,8 @@ fn main() {
 
 #[derive(Default)]
 struct App {
-    window: Option<Window>
+    window: Option<Window>,
+    instance: Option<Instance>
 }
 
 impl ApplicationHandler for App {
@@ -37,7 +55,11 @@ impl ApplicationHandler for App {
             )
             .unwrap();
 
-        self.window = Some(window)
+        let entry = unsafe { Entry::load().expect("Failed to create entry.") };
+        let instance = create_instance(&entry, &window);
+
+        self.window = Some(window);
+        
     }
 
     // Called by `EventLoop::run_app` when a new event happens on the window.
@@ -49,6 +71,124 @@ impl ApplicationHandler for App {
             _ => (),
         }
     }
+}
+
+/// Get the pointers to the validation layers names.
+/// Also return the corresponding `CString` to avoid dangling pointers.
+pub fn get_layer_names_and_pointers() -> (Vec<CString>, Vec<*const c_char>) {
+    let layer_names = REQUIRED_LAYERS
+        .iter()
+        .map(|name| CString::new(*name).unwrap())
+        .collect::<Vec<_>>();
+    let layer_names_ptrs = layer_names
+        .iter()
+        .map(|name| name.as_ptr())
+        .collect::<Vec<_>>();
+    (layer_names, layer_names_ptrs)
+}
+
+unsafe extern "system" fn vulkan_debug_callback(
+    flag: vk::DebugUtilsMessageSeverityFlagsEXT,
+    typ: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _: *mut c_void,
+) -> vk::Bool32 {
+    use vk::DebugUtilsMessageSeverityFlagsEXT as Flag;
+
+    let message = CStr::from_ptr((*p_callback_data).p_message);
+    match flag {
+        Flag::VERBOSE => log::debug!("{:?} - {:?}", typ, message),
+        Flag::INFO => log::info!("{:?} - {:?}", typ, message),
+        Flag::WARNING => log::warn!("{:?} - {:?}", typ, message),
+        _ => log::error!("{:?} - {:?}", typ, message),
+    }
+    vk::FALSE
+}
+
+fn create_debug_create_info() -> vk::DebugUtilsMessengerCreateInfoEXT<'static> {
+    vk::DebugUtilsMessengerCreateInfoEXT::default()
+        .flags(vk::DebugUtilsMessengerCreateFlagsEXT::empty())
+        .message_severity(
+            vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
+                | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                | vk::DebugUtilsMessageSeverityFlagsEXT::INFO,
+        )
+        .message_type(
+            vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+                | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+        )
+        .pfn_user_callback(Some(vulkan_debug_callback))
+}
+
+/// Check if the required validation set in `REQUIRED_LAYERS`
+/// are supported by the Vulkan instance.
+///
+/// # Panics
+///
+/// Panic if at least one on the layer is not supported.
+pub fn check_validation_layer_support(entry: &Entry) {
+    let supported_layers = unsafe { entry.enumerate_instance_layer_properties().unwrap() };
+    for required in REQUIRED_LAYERS.iter() {
+        let found = supported_layers.iter().any(|layer| {
+            let name = unsafe { CStr::from_ptr(layer.layer_name.as_ptr()) };
+            let name = name.to_str().expect("Failed to get layer name pointer");
+            required == &name
+        });
+
+        if !found {
+            panic!("Validation layer not supported: {}", required);
+        }
+    }
+}
+
+fn create_instance(entry: &Entry, window: &Window) -> Instance {
+    let app_name = CString::new("Vulkan Application").unwrap();
+    let engine_name = CString::new("No Engine").unwrap();
+    let app_info = vk::ApplicationInfo::default()
+        .application_name(app_name.as_c_str())
+        .application_version(vk::make_api_version(0, 0, 1, 0))
+        .engine_name(engine_name.as_c_str())
+        .engine_version(vk::make_api_version(0, 0, 1, 0))
+        .api_version(vk::make_api_version(0, 1, 0, 0));
+
+
+    let extension_names =
+        ash_window::enumerate_required_extensions(window.display_handle().unwrap().as_raw())
+                .unwrap();
+    let mut extension_names = extension_names.to_vec();
+
+    if ENABLE_VALIDATION_LAYERS {
+        extension_names.push(debug_utils::NAME.as_ptr());
+    }
+
+    #[cfg(any(target_os = "macos", target_os = "ios"))]
+    {
+        extension_names.push(ash::khr::portability_enumeration::NAME.as_ptr());
+        // Enabling this extension is a requirement when using `VK_KHR_portability_subset`
+        extension_names.push(ash::khr::get_physical_device_properties2::NAME.as_ptr());
+    }
+
+    let (_layer_names, layer_names_ptrs) = get_layer_names_and_pointers();
+
+    let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::default()
+    };
+    let mut debug_create_info = create_debug_create_info();
+    let mut instance_create_info = vk::InstanceCreateInfo::default()
+        .application_info(&app_info)
+        .enabled_extension_names(&extension_names)
+        .flags(create_flags);
+    if ENABLE_VALIDATION_LAYERS {
+        check_validation_layer_support(entry);
+        instance_create_info = instance_create_info
+            .enabled_layer_names(&layer_names_ptrs)
+            .push_next(&mut debug_create_info);
+    }
+
+    unsafe { entry.create_instance(&instance_create_info, None).unwrap() }
 }
 
 
